8c078792dc489eef4c217646c5ef22bc6d2e9582
vmsdaw2425 <vicmarseg3@alu.edu.gva.es>
2025-02-15 13:22:40 +0100

    added dashboard page

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+// app/dashboard/page.js
+export default function DashboardPage() {
+    return (
+      <main className="container mt-4">
+        <h1>Dashboard</h1>
+        <p>Welcome to your dashboard! Here we will display all crypto data, charts, etc.</p>
+      </main>
+    );
+  }
+  
