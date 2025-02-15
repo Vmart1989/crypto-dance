a3c8b2e3646aa825338666294ec7bfbf7cb1f501
vmsdaw2425 <vicmarseg3@alu.edu.gva.es>
2025-02-15 14:14:23 +0100

    Cleaned up initial project structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "CryptoDance",
@@ -20,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <body>
+      {children}
+    </body>
+  </html>
   );
 }
