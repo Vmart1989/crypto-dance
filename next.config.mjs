28887387da7a44c6d874bfcfea896e37c0522800
Vmart1989 <victor.martinezsegura@gmail.com>
2025-03-14 20:12:58 +0100

    fix rewrite 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
-        return [
-          {
-            source: "/:path*",
-            has: [
-              {
-                type: "host",
-                value: "admin.cryptodance.app"
-              }
-            ],
-            destination: "/admin/login"
-          }
-        ];
-      }
-    };
-    
-
-
-export default nextConfig;
-
-
+      return [
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "admin.cryptodance.app"
+            }
+          ],
+          destination: "/admin/login"
+        }
+      ];
+    }
+  };
+  
+  export default nextConfig;
+  
