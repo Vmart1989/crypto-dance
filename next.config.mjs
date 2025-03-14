9f6372f671b1cc234200c3a065aef477aae07dae
Vmart1989 <victor.martinezsegura@gmail.com>
2025-03-14 22:55:16 +0100

    update redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",  // Matches any path under the admin subdomain
+        has: [
+          {
+            type: "host",
+            value: "admin.cryptodance.app"
+          }
+        ],
+        destination: "https://cryptodance.app/admin"  // Redirect any path to cryptodance.app/admin
+      },
+      {
+        source: "/",  // Explicitly handle the root of the admin subdomain
+        has: [
+          {
+            type: "host",
+            value: "admin.cryptodance.app"
+          }
+        ],
+        destination: "https://cryptodance.app/admin"  // Redirect root to cryptodance.app/admin
+      }
+    ];
+  }
 };
 
 export default nextConfig;
