318d0464152725058851d48e386bf7852efea0e8
Vmart1989 <victor.martinezsegura@gmail.com>
2025-03-14 19:52:05 +0100

    added rewrite to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,18 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+{
+    "rewrites": [
+      {
+        "source": "/:path*",
+        "has": [
+          {
+            "type": "host",
+            "value": "admin.cryptodance.app"
+          }
+        ],
+        "destination": "/admin/login"
+      }
+    ]
+  }
