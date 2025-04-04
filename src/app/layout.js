f8947ba0bc5a70350baa51c80ad0eee980d302bb
Vmart1989 <victor.martinezsegura@gmail.com>
2025-04-04 20:59:25 +0200

    body outside head

--- src/app/layout.js
+++ src/app/layout.js
@@ -87,7 +87,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={oswald.className}>
+      
         <head>
           {/* Google Analytics 4 */}
           <Script
@@ -103,16 +103,18 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-48WTNGSD0T');
           `}
           </Script>
-        </head>
-        <link
+          <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
         />
-
-        <Script
+         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           strategy="beforeInteractive"
         />
+        </head>
+        <body className={oswald.className}>
+
+       
         <CurrencyProvider>
           <UserProvider>
             {/* HEADER */}
